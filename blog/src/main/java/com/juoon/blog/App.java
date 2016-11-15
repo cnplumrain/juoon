4b5eb7f39b4c63b3cb2da5e7d146fc87ed0f190f
world <z8414@qq.com>
2016-11-15 23:29:49 +0800

    no changes

--- blog/src/main/java/com/juoon/blog/App.java
+++ blog/src/main/java/com/juoon/blog/App.java
@@ -1,13 +1,9 @@
 package com.juoon.blog;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
