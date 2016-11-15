b32252719653aa46bb172ac7e44b0f3d0ade905b
world <z8414@qq.com>
2016-11-15 23:49:49 +0800

    111

--- blog/src/main/resources/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * 配置资源包
- * @author zhuyongchun
- * @since 2016/11/15
- */
-package resources;
--- src/main/java/com/juoon/blog/App.java
+++ src/main/java/com/juoon/blog/App.java
@@ -16,7 +16,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!ZZZ" );
         SpringApplication.run(App.class, args);
     }
 }
